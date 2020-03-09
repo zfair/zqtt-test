19d6a8bcb5bc996a5d1d14196c56e7afd71895b1
anqurvanillapy <anqurvanillapy@gmail.com>
2020-03-09 16:04:45 +0800

    Updated server port

--- mqtt_test.go
+++ mqtt_test.go
@@ -1,20 +1,20 @@
 package test
 
 import (
+	Mqtt "github.com/eclipse/paho.mqtt.golang"
 	"testing"
-	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
-const endpoint = "tcp://127.0.0.1:12345"
+const endpoint = "tcp://127.0.0.1:1883"
 
 func TestConnect(t *testing.T) {
-	opts := MQTT.NewClientOptions()
+	opts := Mqtt.NewClientOptions()
 	opts.AddBroker(endpoint)
 	opts.SetClientID("0")
 	opts.SetUsername("test")
 	opts.SetPassword("test")
 
-	client := MQTT.NewClient(opts)
+	client := Mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		t.Fatal(token.Error())
 	}
